53f5422716543034f82f8a66735048254b7ad8d9
Sensess <SensessYentzer@outlook.com>
2022-07-13 15:38:13 -0400

    Updates

--- Server/schemas/resolvers.js
+++ Server/schemas/resolvers.js
@@ -5,28 +5,44 @@ const { signToken } = require('../utils/auth');
 //Resolver to Get thoughts 
 const resolvers = {
   Query: {
-    thoughts: async (parent, {username}) => {
+    thoughts: async (parent, { username }) => {
       const params = username ? { username } : {}
-      return Thought.find().sort({ createdAt: -1});
+      return Thought.find().sort({ createdAt: -1 });
     },
-    thought: async (parent, {_id}) => {
-      return Thought.findOne({_id});
+    thought: async (parent, { _id }) => {
+      return Thought.findOne({ _id });
     },
     // get all users
-users: async () => {
-  return User.find()
-    .select('-__v -password')
-    .populate('friends')
-    .populate('thoughts');
-},
-// get a user by username
-user: async (parent, { username }) => {
-  return User.findOne({ username })
-    .select('-__v -password')
-    .populate('friends')
-    .populate('thoughts');
-}
-}
+    users: async () => {
+      return User.find()
+        .select('-__v -password')
+        .populate('friends')
+        .populate('thoughts');
+    },
+    // get a user by username
+    user: async (parent, { username }) => {
+      return User.findOne({ username })
+        .select('-__v -password')
+        .populate('friends')
+        .populate('thoughts');
+      }
+    },
+
+    //Mutations
+
+   Mutation: {
+      //Here the Mongoose User model creates a new user
+      //in the db with whatever is passed in args
+      addUser: async (parent, args) => {
+        const user = await User.create(args);
+        return user;
+      },
+      login: async () => {
+
+      
+
+    }
+  }
 };
 
 
